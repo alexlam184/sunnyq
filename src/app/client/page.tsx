e85ff5bbc3a4b6bbec3c4cccddfd3f7b10079bcb
alexlam184 <alexwork0211@gmail.com>
2024-05-13 15:23:42 -0700

    first commit

---
+++ src/app/client/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function ClientPage() {
+  return (
+    <div>
+      <title className='text-white'>Client page</title>
+      <div>
+        <p>This is Client page</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/host/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function HostPage() {
+  return (
+    <div>
+      <title>Host page</title>
+      <div>
+        <p>This is Host page</p>
+      </div>
+    </div>
+  );
+}
