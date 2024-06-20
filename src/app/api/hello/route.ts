ad85b67c9456fa92be6c58a76a8ce5a3d9487842
alexlam184 <alexwork0211@gmail.com>
2024-06-20 07:36:10 -0700

    test : add api test

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+// Inside app/api/route.ts
+// This will create end point GET http://localhost:3000/api
+
+// Test this like http://localhost:3000/api/hello?name=Meera
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const name = searchParams.get('name');
+
+  return NextResponse.json({ name });
+}
+
+// This will create end point POST http://localhost:3000/api/hello
+
+export async function POST(request: NextRequest) {
+  try {
+    const json = await request.json();
+
+    return NextResponse.json(json);
+  } catch (e) {
+    console.log(e);
+    return new Response(null, { status: 400, statusText: 'Bad Request' });
+  }
+}
