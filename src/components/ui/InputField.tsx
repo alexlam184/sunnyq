b047a8b97aaaae34e06b2dcce91ed861f2641117
listenrwt <listenrwtprivate@gmail.com>
2024-07-15 18:40:36 +0800

    refactor: modify inputfield style

--- src/components/ui/InputField.tsx
+++ src/components/ui/InputField.tsx
@@ -14,7 +14,7 @@ const InputField: React.FC<InputProps> = ({
   defaultValue,
 }) => {
   return (
-    <label className='relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600'>
+    <label className='bg-white relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600'>
       <input
         type={type}
         id={title}
