3502f8aea6d1ee2d03f6d2b054423674ec58c8ca
alexlam184 <alexwork0211@gmail.com>
2024-08-03 00:17:43 -0700

    build : change production backend url

--- src/lib/socket/socketio.service.ts
+++ src/lib/socket/socketio.service.ts
@@ -3,8 +3,12 @@
 import { io } from 'socket.io-client';
 const hostname = process.env.NEXT_PUBLIC_SOCKETIO_HOSTNAME;
 const port = process.env.NEXT_PUBLIC_SOCKETIO_PORT;
-const socketio_url = `http://${hostname}:${port}`;
+const socketio_url =
+  process.env.NEXT_PUBLIC_NODE_ENV == 'production'
+    ? process.env.NEXT_PUBLIC_SOCKETIO_SERVER_URL!
+    : `http://${hostname}:${port}`;
 
+console.log(`Environment: ${process.env.NEXT_PUBLIC_NODE_ENV}`);
 export const socket = io(socketio_url, {
   withCredentials: true,
   extraHeaders: {
