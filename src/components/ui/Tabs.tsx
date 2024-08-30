67bcc71c299dac19041bb255dbf91726b316f8ac
listenrwt <listenrwtprivate@gmail.com>
2024-08-30 13:29:33 +0800

    Fix: Prevent null selection cases with available options.

--- src/components/ui/Tabs.tsx
+++ src/components/ui/Tabs.tsx
@@ -5,27 +5,31 @@ export interface TabOption {
   value: any;
 }
 
-interface TabProps {
-  name?: string | '';
-  options: TabOption[];
-  onChange?: ((selectedOption: TabOption) => void) | undefined;
-  defaultValue?: any;
-  disabledValues?: any[];
-}
-
-const TabComponent: React.FC<TabProps> = ({
+const TabComponent = ({
   name,
   options,
   onChange,
   defaultValue,
   disabledValues,
+  themeColor = 'blue',
+}: {
+  name?: string | '';
+  options: TabOption[];
+  onChange?: ((selectedOption: TabOption) => void) | undefined;
+  defaultValue?: any;
+  disabledValues?: any[];
+  themeColor?: 'blue' | 'green' | 'red';
 }) => {
-  const [selectedOption, setSelectedOption] = useState<TabOption | null>(
-    options.find((option) => option.value === defaultValue) || null
+  const [selectedOption, setSelectedOption] = useState<TabOption | undefined>(
+    options.find((option) => option.value === defaultValue) || undefined
   );
 
   useEffect(() => {
-    if (selectedOption && disabledValues?.includes(selectedOption.value)) {
+    if (
+      selectedOption &&
+      (!options.some((option) => option.value === selectedOption.value) ||
+        disabledValues?.includes(selectedOption.value))
+    ) {
       // Find the next available option that is not disabled
       const nextOption = options.find(
         (option) => !disabledValues?.includes(option.value)
@@ -38,10 +42,22 @@ const TabComponent: React.FC<TabProps> = ({
     }
   }, [selectedOption, disabledValues, options, onChange]);
 
+  const bgColorVariant = {
+    blue: 'bg-blue-500',
+    green: 'bg-green-500',
+    red: 'bg-red-500',
+  };
+
+  const selectedColorVariant = {
+    blue: 'text-blue-600 border-blue-500 border-b-2',
+    green: 'text-green-600 border-green-500 border-b-2',
+    red: 'text-red-600 border-red-500 border-b-2',
+  };
+
   return (
     <div className='flex relative overflow-x-auto overflow-y-hidden border-gray-200 whitespace-nowrap dark:border-gray-700 justify-evenly'>
       <div
-        className={`absolute bottom-0 h-0.5 bg-blue-500 transition-all duration-300 ease-in-out`}
+        className={`absolute bottom-0 h-0.5 ${bgColorVariant[themeColor]} transition-all duration-300 ease-in-out`}
       />
       {options.map((option: TabOption, index) => {
         const disabled: boolean =
@@ -61,7 +77,7 @@ const TabComponent: React.FC<TabProps> = ({
               disabled
                 ? 'text-gray-300'
                 : option.value === selectedOption?.value
-                  ? 'text-blue-600 border-b-2 border-blue-500'
+                  ? selectedColorVariant[themeColor]
                   : 'text-gray-700'
             } sm:text-base dark:text-blue-300 whitespace-nowrap focus:outline-none`}
             disabled={disabled}
